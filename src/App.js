13fb5c3654f2f57c5e56de982dc9910fdad16169
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-08-30 21:02:41 -0400

    My Final Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Error404 from "./components/error404/Error404"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Switch>
+      <Route
+       exact
+        path="/" 
+        render={(props) => <Welcome {...props} name="Georgette"/>} 
+        />
+        <Route
+          exact
+          path="/welcome/:name" 
+          render={(props) => <Welcome {...props} name={props.match.params.name}/>}
+          />
+      <Route path="/clock" component={Clock} />
+      <Route path="/contact" component={Contact} />
+      <Route path="*">
+        <Error404/>
+        </Route>
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Welcome = (props) => (
+    <div className="Welcome">
+        <h3>Welcome, {props.name} </h3>
+    </div>
+)
+export default Welcome
